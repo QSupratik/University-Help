b2b3e3b9c6d11cc4c1fd47c56ffc2f77d9eb30e5
QSupratik <supratikdatta2k20@gmail.com>
2025-07-27 11:46:29 +0530

    User routes done

--- Chatapp/backend/src/controllers/user.controller.js
+++ Chatapp/backend/src/controllers/user.controller.js
@@ -1,6 +1,5 @@
 import User from "../models/User.js";
 import FriendRequest from "../models/FriendRequest.js";
-import { response } from "express";
 
 export async function getRecommendedUsers(req, res){
     try{
@@ -80,3 +79,68 @@ export async function sendFriendRequest(req, res){
     }
 }
 
+export async function acceptFriendRequest(req, res){
+    try {
+        const {id:requestId} = req.params;
+        const friendRequest = await FriendRequest.findById(requestId);
+
+        if(!friendRequest){
+            return res.status(404).json({message:"Friend request not found"});
+        }
+
+        //Checking if the current user is the actual recipient
+        if(friendRequest.recipient.toString()!=req.user.id){
+            return res.status(403).json({message:"You are not authorised to accept the request"});
+        }
+
+        friendRequest.status = "accepted";
+        await friendRequest.save();
+
+        //Add each user to the other's friend's Array
+        await User.findByIdAndUpdate(friendRequest.sender,{
+            $addToSet:{friends:friendRequest.recipient}
+        });
+
+        await User.findByIdAndUpdate(friendRequest.recipient,{
+            $addToSet:{friends:friendRequest.sender}
+        });
+
+        res.status(200).json({message:"Friend request accepted"});
+    } catch (error) {
+        console.log("Error in acceptFriendRequest constroller", error.message);
+        res.status(500).json({message:"Intermal Server Error"});
+    }
+}
+
+export async function getFriendRequests(req, res){
+    try {
+        const incomingReqs = await FriendRequest.find({
+            recipient:req.user.id,
+            status:"pending"
+        }).populate("sender", "fullName profilePic nativeLanguage learningLanguage");
+        
+        const acceptedReqs = await FriendRequest.find({
+            sender:req.user.id,
+            status:"accepted"
+        }).populate("recipient", "fullName profilePic");
+
+        res.status(200).json({incomingReqs, acceptedReqs});
+    } catch (error) {
+        console.log("Error in getPendingFriendRequests controller\n", error.message);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+export async function getOutgoingFriendReqs(req, res){
+    try {
+        const outgoingRequests = await FriendRequest.find({
+            sender:req.user.id,
+            status:"pending"
+        }).populate("recipient", "fullName profilePic nativeLanguage learningLanguage");
+        
+        res.status(200).json(outgoingRequests);
+    } catch (error) {
+        console.log("Error in getOutgoingFriendRequests controller\n", error.message);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+}
--- Chatapp/backend/src/routes/user.route.js
+++ Chatapp/backend/src/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware";
-import { sendFriendRequest, getRecommendedUsers, getMyFriends } from "../controllers/user.controller";
+import { sendFriendRequest, getRecommendedUsers, getOutgoingFriendReqs,
+    getMyFriends, acceptFriendRequest, getFriendRequests } from "../controllers/user.controller";
 const router = express.Router();
 
 router.use(protectRoute);
@@ -8,6 +9,10 @@ router.use(protectRoute);
 router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
 
-router.post("friend-request/:id", sendFriendRequest);
+router.post("/friend-request/:id", sendFriendRequest);
+router.post("/friend-request/:id/accept", acceptFriendRequest);
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
+
 
 export default router;
