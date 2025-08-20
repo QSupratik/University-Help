2210aa1508fb10608eac0312b192da919e7d94ec
QSupratik <supratikdatta2k20@gmail.com>
2025-08-21 02:37:19 +0530

    Frontend initial

--- Chatapp/backend/src/server.js
+++ Chatapp/backend/src/server.js
@@ -1,8 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 
@@ -11,6 +13,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(express.json());
 app.use(cookieParser());
 
